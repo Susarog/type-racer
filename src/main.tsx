75f423bbcdc5ec38e66c068eda0190c73d4aae3a
Ryan Hang <ryanhang92802@gmail.com>
2023-06-03 18:46:39 -0700

    Add React-Router-Dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Root from "./root";
+import ErrorPage from "./error-page";
+import Content from "./components/Content";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./styles/main.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+      <Route errorElement={<ErrorPage />}>
+        <Route path="/" element={<Content />} />
+        <Route path="/profile" element={<div>temp</div>} />
+      </Route>
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
